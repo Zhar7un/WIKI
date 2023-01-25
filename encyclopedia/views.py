cd6c00aec58152445aa16721396a0d4c4d50b465
Zhartun <gulyaevdima1998@gmail.com>
2023-01-25 17:37:49 +0200

    Add links for articles

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2 as md
 
 from . import util
 
@@ -7,4 +8,16 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def render_page(request, title):
+    if util.get_entry(title):
+        return render(request, "encyclopedia/article.html", {
+            "title": title,
+            "entry": md.markdown(util.get_entry(title))
+        })
+    return render(request, "encyclopedia/article_not_found.html", {
+             "title": title
+        })
+
 
